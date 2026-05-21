3726a8a24bc41498569ef18bba7f76ffe295d73b
Mauricio <castromac6367@gmail.com>
2026-05-20 21:02:35 -0300

    Add loop exercises

---
+++ python/senha.py
@@ -0,0 +1,9 @@
+
+while True:
+    senha = input("Digite a senha: ")
+
+    if senha != "1234":
+        print("Senha incorreta. Tente novamente.")
+    else:
+        print("Senha correta. Acesso concedido.")
+        break
